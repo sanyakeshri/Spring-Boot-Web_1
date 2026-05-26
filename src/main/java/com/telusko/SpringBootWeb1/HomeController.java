683d0bd6f92bf3c29b8cf9cb0ff49ca56d7c2d3f
Sanya keshri <keshrisanya621@gmail.com>
2026-05-27 02:49:33 +0530

    model&view

--- src/main/java/com/telusko/SpringBootWeb1/HomeController.java
+++ src/main/java/com/telusko/SpringBootWeb1/HomeController.java
@@ -3,8 +3,10 @@ package com.telusko.SpringBootWeb1;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -13,24 +15,32 @@ public class HomeController {
     @RequestMapping("/")
     public String home() {
         System.out.println("Home method called");
-        return "index.jsp";
+        return "index";
     }
 
     @RequestMapping("add")
 //    public String add(HttpServletRequest req , HttpSession session){
 
-        public String add(@RequestParam("num1") int num, int num2  , HttpSession session){
+        public ModelAndView add(@RequestParam("num1") int num, int num2  , Model model){
+
 
 //        int num1 = Integer.parseInt(req.getParameter("num1"));
 //        int num2 = Integer.parseInt(req.getParameter("num2"));
-        int result = num + num2 + 7;
+          int result = num + num2 + 12;
 
-        session.setAttribute("result",result);
+//        session.setAttribute("result",result);
 
-        System.out.println(result);
+//        model.addAttribute("result" , result);
+
+        // Create ModelAndView object
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("result" , result);
+        mv.setViewName("result");
+
+//        System.out.println(result);
 
 //        System.out.println("in add");
-         return "result.jsp";
+         return mv ;
    }
 
 }
