3224d81c52efff59b3d1a102d80fe52a2c242319
Sanya keshri <keshrisanya621@gmail.com>
2026-05-27 04:14:11 +0530

    AddingModelAttribute

--- src/main/java/com/telusko/SpringBootWeb1/HomeController.java
+++ src/main/java/com/telusko/SpringBootWeb1/HomeController.java
@@ -7,12 +7,20 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
+
+    //MAKING METHOD FOR COUSE NAME:
+
+    @ModelAttribute("Course")
+    public String courseName(){
+        return "JAVA";
+    }
 
 
     @RequestMapping("/")
@@ -22,33 +30,42 @@ public class HomeController {
     }
 
     @RequestMapping("addAlien")
-//    public String add(HttpServletRequest req , HttpSession session){
 
-        public ModelAndView addAlien(@RequestParam("aid") int aid,@RequestParam("aname") String aname  , ModelAndView model){
+//    IT ALSO WORKS WITHOUT MODEL ATTRIBUTE,IT'S OPTIONAL:
+    public String add(@ModelAttribute("alien1") Alien alien){
 
 
-//        int num1 = Integer.parseInt(req.getParameter("num1"));
-//        int num2 = Integer.parseInt(req.getParameter("num2"));
-//          int result = num + num2 + 12;
+//  public String add(HttpServletRequest req , HttpSession session){
 
-//        session.setAttribute("result",result);
+//  public ModelAndView addAlien(@RequestParam("aid") int aid,@RequestParam("aname") String aname  , ModelAndView model){
 
-//        model.addAttribute("result" , result);
 
-        Alien alien = new Alien();
-        alien.setAid(aid);
-        alien.setAname(aname);
+//  int num1 = Integer.parseInt(req.getParameter("num1"));
+//  int num2 = Integer.parseInt(req.getParameter("num2"));
+//  int result = num + num2 + 12;
+
+//  session.setAttribute("result",result);
+
+//  model.addAttribute("result" , result);
+
+//        OPTIONAL STEPS:
+//  Alien alien = new Alien();
+//  alien.setAid(aid);
+//  alien.setAname(aname);
 
         // Create ModelAndView object
-       ModelAndView mv = new ModelAndView();
 
-        mv.addObject("alien" , alien);
-        mv.setViewName("result");
+//       ModelAndView mv = new ModelAndView();
+//
+//        mv.addObject("alien" , alien);
+//        mv.setViewName("result");
 
 //        System.out.println(result);
 
 //        System.out.println("in add");
-         return mv ;
+
+
+         return "result" ;
    }
 
 }
--- src/main/webapp/views/result.jsp
+++ src/main/webapp/views/result.jsp
@@ -5,11 +5,10 @@
         <link rel="stylesheet" type="text/css" href="style.css">
     </head>
     </body>
-        //<h2>Result is : <%= session.getAttribute("result") %>  </h2>
-        //OR
-        //<h2>Result is : ${result} </h2>
 
         <h2>Welcome To Telusko</h2>
-        <p>Result is : ${alien}<p>
+        <p>Result is : ${alien1}<p>
+        <h3>Welcome to the ${Course} world</h3>
+
     </body>
 </html>
